f5e925de646568a2edcf161981c18238aae9a78b
Misha <bobyyy239@gmail.com>
2025-08-29 21:28:37 +0300

    feat(post_gen_project): realised

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,30 @@
+import os
+import shutil
+
+print('Project created successfully!')
+print('Next steps:')
+print('0. Uncomment string "# .env  # TODO: uncomment! " in .gitignore')
+print('1. cd {{cookiecutter.project_slug}}')
+print('2. git init')
+print('3. git add .')
+print('4. git commit -m "Initial commit"')
+print('5. git branch -M main')
+print('6. git remote add origin {Your repo}')
+print('7. git push -u origin main')
+print('8. git switch -c develop')
+
+# Remove files and directories not needed by the user
+
+if '{{ cookiecutter.use_todo_md }}' != 'y':
+    os.remove('TODO.md')
+if '{{ cookiecutter.use_gitlab }}' != 'y':
+    shutil.rmtree('.gitlab')
+    os.remove('.gitlab-ci.yml')
+
+# License
+license_ = "{{ cookiecutter.license }}"
+if license_ in {"MIT", "BSD", "Apache", "GPL"}:
+    shutil.move('template_data/licenses/{{ cookiecutter.license }}', 'LICENSE')
+
+# Remove redundant files and directories
+shutil.rmtree('template_data')
